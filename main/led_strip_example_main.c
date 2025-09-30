fc444f3fef36e486e6adac39fa801d775610f004
Andrey Litvichenko <comradchegm@gmail.com>
2025-09-30 14:15:53 +0500

    Change the RMT_LED_STRIP_GPIO_NUM

--- main/led_strip_example_main.c
+++ main/led_strip_example_main.c
@@ -11,9 +11,9 @@
 #include "led_strip_encoder.h"
 
 #define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
-#define RMT_LED_STRIP_GPIO_NUM      0
+#define RMT_LED_STRIP_GPIO_NUM      16
 
-#define EXAMPLE_LED_NUMBERS         24
+#define EXAMPLE_LED_NUMBERS         72
 #define EXAMPLE_CHASE_SPEED_MS      10
 
 static const char *TAG = "example";
